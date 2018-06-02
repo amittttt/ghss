20d66f39b3ab6fc55d14ef0e4977cdf048b3aa6b
Amit Mishra <amittttmishra@gmail.com>
2018-06-02 21:40:41 +0530

    Add files via upload

---
+++ blog/js/functions.js
@@ -0,0 +1,17 @@
+ function myFunction(x) {
+     x.classList.toggle("change");
+     var x = document.getElementById('data-blocks');
+     var y = document.getElementById('ad-after-header');
+     if (x.style.visibility === 'visible') {
+        // y.style.visibility = 'visible';
+         x.style.visibility = 'hidden';
+     } else {
+        // y.style.visibility = 'hidden';
+         x.style.visibility = 'visible';
+     }
+ }
+
+ // Contact page JS start
+
+ 
+ // Contact page Js End
